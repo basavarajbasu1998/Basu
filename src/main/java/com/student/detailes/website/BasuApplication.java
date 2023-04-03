529173d5fc940809e841937035f81ad7847a155f
basavarajbasu1998 <bhosamani543@gmail.com>
2023-04-03 11:19:03 +0530

    commit

---
+++ src/main/java/com/student/detailes/website/BasuApplication.java
@@ -0,0 +1,13 @@
+package com.student.detailes.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasuApplication.class, args);
+	}
+
+}
